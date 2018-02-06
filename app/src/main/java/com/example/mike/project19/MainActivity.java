125670e9d52b6cd5e24900eaacd332fd34f66918
Mike G <gml29@yandex.ru>
2018-02-06 17:26:03 +0300

    19_1

--- app/src/main/java/com/example/mike/project19/MainActivity.java
+++ app/src/main/java/com/example/mike/project19/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +20,24 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.actionSettings:
+                Toast.makeText(this, "Настройки", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.actionSearch:
+                Toast.makeText(this, "Поиск", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.actionExit:
+                Toast.makeText(this, "Выход", Toast.LENGTH_LONG).show();
+                finish();
+                break;
+                /*  */
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
